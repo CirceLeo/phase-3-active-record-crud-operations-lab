dac309506d87ba1175d729e903681bb12c0934d1
Calypso Leonard <calypsorleonard@gmail.com>
2022-04-11 14:42:37 -0400

    most tests pass

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -10,7 +10,7 @@ class Movie < ActiveRecord::Base
     def self.last_movie
         Movie.last
     end
-    def self.movie_count #fix
+    def self.movie_count 
         Movie.count
     end
     def self.find_movie_with_id id
